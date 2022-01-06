aa4e026ca134d19b6a953080fef7543a456fab6e
Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2022-01-06 16:43:45 -0100

    Update Floresta.java

--- Floresta.java
+++ Floresta.java
@@ -2,6 +2,72 @@ package floresta;
 
 import java.util.Arrays;
 
+class Grafoo{
+	int[][] matrix; 
+	
+	public Grafoo(int n) {
+		this.matrix = new int[n][n];
+	}
+	
+	
+	// nao orientado 
+	public void add_edge( int m, int n, int p) {
+		this.matrix[n][m] = p;
+		this.matrix[m][n] = p;
+	}
+
+	@Override
+	public String toString() {
+		return "Grafo : Adj matrix = " + Arrays.deepToString(matrix);
+	}
+
+public static int findMaxVertex(boolean[] visited, int[] weights, Grafoo graph) {
+	
+	int v = graph.matrix.length;
+	int index = -1;	
+	int maxW = Integer.MIN_VALUE;
+		
+	for (int i =0; i<v; i++) {
+			
+		if (visited[i]== false && weights[i] > maxW)
+			maxW = weights [i];
+			index = i;
+		}
+	return index;
+	}
+	
+public static void maximumSpanningTree(Grafoo graph) {
+	int v = graph.matrix.length;
+	
+	boolean[] visited = new boolean [v];
+	int[] weights = new int[v];
+	int[] parent = new int[v];
+	
+	for(int i =0; i<v; i ++) {
+		visited[i] =false;
+		weights[i] = Integer.MIN_VALUE;
+	}	
+	
+	// obriga primeiro elemento a ser a raiz
+	weights[0] =Integer.MAX_VALUE;
+	parent[0] = -1;
+	
+	for( int i = 0; i < v-1; i++) {
+		
+		int maxVertexIndex = findMaxVertex(visited, weights, graph);
+		visited[maxVertexIndex] = true;
+		 for (int j = 0; j < v; j++) {
+			 if (graph.matrix[j][maxVertexIndex] != 0 && visited[j] == false) {
+				 if (graph.matrix[j][maxVertexIndex] > weights[j]) {
+					 weights[j] = graph.matrix[j][maxVertexIndex];
+					 parent[j] = maxVertexIndex;
+				 	}
+			 	}
+		 	}
+		}			
+	}
+	}
+
 public class Forest {
 	int[] nos;
 	//cada posicao representa um no e cada valor é o index do seu pai
@@ -11,16 +77,12 @@ public class Forest {
 		for(int i =0; i<this.nos.length; i++) {
 			this.nos[i] = -1;
 		}
-	}
-	
+	}	
 	
 	// torna m o pai de n 
 	public void set_parent(int n, int m) {
 		this.nos[n] = m;
 	}
-	
-	
-	
 	
 	/**
 	 * @mariaclaramenezes
@@ -44,14 +106,10 @@ public class Forest {
 		return b;
 	}
 	
-	
-	
 	@Override
 	public String toString() {
 		return "Forest [nos=" + Arrays.toString(nos) + "]";
 	}
-
-
 	public static void main(String[] args) {
 		Forest forest = new Forest(15);
 		System.out.println(forest);
@@ -67,5 +125,23 @@ public class Forest {
 		System.out.println(simple_forest);
 		System.out.println(simple_forest.treeQ());
 		
+		Grafoo graph = new Grafoo(5);
+		
+		graph.add_edge(1,0,2);
+		graph.add_edge(3,0,6);
+		graph.add_edge(2,1,3);
+		graph.add_edge(2,4,7);
+		graph.add_edge(3,1,8);
+		graph.add_edge(3,4,9);
+		graph.add_edge(4,1,5);
+		
+//		{ { 0, 2, 0, 6, 0 },
+//		{ 2, 0, 3, 8, 5 },
+//	      { 0, 3, 0, 0, 7 },
+//	      { 6, 8, 0, 0, 9 },
+//	      { 0, 5, 7, 9, 0 } };
+		
+		System.out.println(graph);
+		Grafoo.maximumSpanningTree(graph);
+		}	
 	}
-}
