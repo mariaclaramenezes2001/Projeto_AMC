5ef6a66f92a10024a49ac08438a1686a976d35ea
Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2022-01-15 18:45:03 +0000

    Update BN.java

--- BN.java
+++ BN.java
@@ -1,46 +1,90 @@
 
 public class BN {
-	Forest MST;
-	// lista de tabelas com tetas ? 
+	public Forest arvore;
+	public ArrayList<double[][]> DFOvs;
+	public double[] DFOc;
 	
-	
-	public double[][] DFO_var(Forest MTS, Amostra amostra, int no, int s) {
+	public static double[][] DFO_var(Forest MTS, Amostra amostra, int no, double s) {
 		//numero de linhas = numero de valores possiveis para o pai do no = domain_aux
+		
 		int pai = MTS.list[no];
-		int m = Amostra.domain_aux(amostra,pai);
+		int m = Amostra.domain_aux(amostra, pai);
 		
 		//numero de colunas = numero de possiveis valores do no
 		int n = Amostra.domain_aux(amostra,no);
 		
 		double[][] dfo_var = new double[m][n];
 		
-		// pode ser evitado se fizer overload de count para valores unicos
-		int[] var = {no, pai};
-		int[] var_pai = {pai};
+		int[] XY = {no, pai};
+		//int[] Y = {pai};
 		
+	
 		for (int i = 0; i < m; i++ ) {
 			for ( int j = 0; j < n ; j++) {
 				
-				int[] val_pai = {i};
-				int[] val = {i,j};
-				dfo_var[i][j] = (amostra.count(var,val) + s) / (amostra.count(var_pai, val_pai) + s*n );
+				
+				int[] xy = {j,i};
+				
+				dfo_var[i][j] = (amostra.count(XY , xy) + s) / (amostra.count(pai, i) + s*n );
 			}
 		}
 		return dfo_var;}
 		
 		
-		public double[] DFO_class(Amostra amostra, int s) {
+		public double[] DFO_class(Amostra amostra, double s) {
 			double[] dfo_class = new double[2];
 			int[] class_index = {amostra.getList().get(0).length-1};
 			int[] class0 = {0};
 			int[] class1 = {1};
 			
-			dfo_class[0] = (amostra.count(class_index, class0) + s) / ( 2*s);
-			dfo_class[1] = (amostra.count(class_index, class1) + s) / ( 2*s);
+			dfo_class[0] = (amostra.count(class_index, class0) + s) / (  amostra.length()+ 2*s);
+			dfo_class[1] = (amostra.count(class_index, class1) + s) / (  amostra.length() + 2*s);
+
+			return dfo_class;			
+		}
+		
+
+		public BN(Forest MSTree, Amostra amostra, double s){
+			this.arvore = MSTree;
+			this.DFOc = DFO_class(amostra,s);
 			
-			return dfo_class;
+			int colunas = amostra.element(0).length;
+			
+			ArrayList<double[][]> dfovs = new ArrayList<double[][]>(colunas);		
+			for ( int i = 0; i <colunas-1; i ++) {
+				dfovs.add(DFO_var(MSTree, amostra, i, s));
+			}
+			
+			this.DFOvs = dfovs;		
+			
+			
 		}
 		
 		
 	
+
+		@Override
+		public String toString() {
+			
+			StringBuilder toPrint = new StringBuilder();
+			toPrint.append("BN\n [Arvore=" + arvore);
+			
+			for ( double[][] table : DFOvs) {
+				toPrint.append(Arrays.deepToString(table));
+			}
+			toPrint.append(Arrays.toString(DFOc) + "]");
+			return toPrint.toString();
+		}
+
+
+		public static void main(String[] args) {
+			
+			Amostra amostra_tiroide = new Amostra("thyroid.csv");
+			Grafo grafoP_tiroide = Grafo.grafoP(amostra_tiroide);
+			Forest MSTree_tiroide = MST.maximumSpanningTree(grafoP_tiroide);
+
+			
+			BN bn_tiroide = new BN(MSTree_tiroide, amostra_tiroide,0.5);
+			System.out.println( bn_tiroide);		
+		}		
 }
