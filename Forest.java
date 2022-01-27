c5b4b9c526ae9602602baf9648da18457084f5aa
Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2022-01-27 01:52:35 +0000

    Update Forest.java

--- Forest.java
+++ Forest.java
@@ -1,12 +1,16 @@
 
-
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Forest implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	int[] list;
+	int[] domains; 
+
+	
 	//cada posicao representa um no e cada valor é o index do seu pai
 	
 	public Forest(int n) {
@@ -21,25 +25,53 @@ public class Forest implements Serializable {
 		this.list[n] = m;
 	}
 	
-	/**
-	 * @mariaclaramenezes
-	 * 
-	 * todos os nós têm no máximo um pai, com excepção da raiz.
-	 * a função set parent ja esta a assegurar parte disso, ver se a floresta é uma so arvore unica
-	 * 
-	 * ver se tem mais que uma raiz 
-	 */
+	
+	// lista de vertices que sao filhos do vertice o 
+	public LinkedList<Integer> offspring(int o){
+				
+		LinkedList<Integer> offspring = new LinkedList<Integer>();
+		for (int d= 0; d<this.list.length ;d++ ) {
+			if (this.list[d] == o) 
+				offspring.add(d);
+		}
+		return offspring;
+	}	
+
+	public LinkedList<Integer> BFS (int o){
+		LinkedList<Integer> visited = new LinkedList<Integer>();
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(o);	
+		while(!queue.isEmpty()) {
+			int first  = queue.remove();
+			if (!visited.contains(first)) {
+				visited.add(first);
+				queue.addAll(offspring(first));
+			}
+		}
+		visited.removeFirst();
+		return visited; }
+	
+	public boolean cylicQ(int o) {
+		LinkedList<Integer> desc = BFS(o);
+		return desc.contains(o);
+	}
 	
 	
+	// so uma raiz 
+		//  - excepção: casos com dominio = 1
+	// nao ter ciclos : impossivel aceder ao vertice de origem atraves das arestas sucessivas - bfs
+		
 	public boolean treeQ() {
 		int s = 0; 
-		boolean b = true;
-		
-		for (int x: this.list) {
-			if (x == -1) s++;}
-			
-		if( s != 1 ) b = false; 
-	
+		boolean b = true;	
+		for (int i = 0 ; i < this.list.length; i ++) {
+			if (this.list[i] == -1 || ( this.list[i] == i && this.domains[i] > 1)) {
+				s++;  
+				}
+			if (this.domains[i] > 1 && cylicQ(i) ) b = false; }					
+		if( s != 1 ) {
+				b = false;	 
+			}					
 		return b;
 	}
 	
@@ -48,69 +80,27 @@ public class Forest implements Serializable {
 		return "Forest [list=" + Arrays.toString(list) + "]";
 	}
 	public static void main(String[] args) {
-		Forest forest = new Forest(15);
-		System.out.println(forest);
-		forest.set_parent(0,3);
-		System.out.println(forest);
-		System.out.println(forest.treeQ());
 		
-		Forest simple_forest = new Forest(5); // slide 14 , C=0
-		simple_forest.set_parent(0,3);
-		simple_forest.set_parent(2,1);
-		simple_forest.set_parent(3,1);
-		simple_forest.set_parent(4,1);
-		System.out.println(simple_forest);
-		System.out.println(simple_forest.treeQ());
+		Amostra amostra_tiroide = new Amostra("thyroid.csv");
+		Grafo grafoP_tiroide = Grafo.grafoP(amostra_tiroide);
+		Forest MSTree_tiroide = MST.maximumSpanningTree(grafoP_tiroide);
+		System.out.println(MSTree_tiroide.treeQ());
 		
-		System.out.println();
-		System.out.println();
-		System.out.println("NEW VERSION OF FLOREST");
+		Amostra amostra_bcb= new Amostra("bcancerboost.csv");
+		Grafo grafoP_bcb = Grafo.grafoP(amostra_bcb);
+		Forest MSTree_bcb = MST.maximumSpanningTree(grafoP_bcb);
+		System.out.println(MSTree_bcb.treeQ());
 		
+		Amostra amostra_d = new Amostra("diabetes.csv");
+		Grafo grafoP_d = Grafo.grafoP(amostra_d);
+		Forest MSTree_d = MST.maximumSpanningTree(grafoP_d);
+		System.out.println(MSTree_d.treeQ());
 		
+		Amostra amostra_soy = new Amostra("soybean-large.csv");
+		Grafo grafoP_soy = Grafo.grafoP(amostra_soy);
+		Forest MSTree_soy = MST.maximumSpanningTree(grafoP_soy);
+		System.out.println(MSTree_soy.treeQ());
 		
-		Grafo graph = new Grafo(5);
-		
-		graph.add_edge(1,0,2);
-		graph.add_edge(3,0,6);
-		graph.add_edge(2,1,3);
-		graph.add_edge(2,4,7);
-		graph.add_edge(3,1,8);
-		graph.add_edge(3,4,9);
-		graph.add_edge(4,1,5);
-		
-//	  { { 0, 2, 0, 6, 0 },
-//		{ 2, 0, 3, 8, 5 },
-//	    { 0, 3, 0, 0, 7 },
-//	    { 6, 8, 0, 0, 9 }, 
-//	    { 0, 5, 7, 9, 0 } };
-		System.out.println();
-		
-		System.out.println(graph);
-		System.out.println();
-	
-
-		Forest mst = MST.maximumSpanningTree(graph);
-		System.out.println("Maximum Spanning Tree  " + mst);
-		System.out.println("Is this result a tree?: " + mst.treeQ());
-		
-		Grafo prim = new Grafo(6);
-		prim.add_edge(0,1,6);
-		prim.add_edge(0,2,1);
-		prim.add_edge(0,3,5);
-		prim.add_edge(1,2,2);
-		prim.add_edge(1,4,5);
-		prim.add_edge(2,3,2);
-		prim.add_edge(2,4,6);
-		prim.add_edge(2,5,4);
-		prim.add_edge(3,5,4);
-		prim.add_edge(4,5,3);
-		
-		Forest mst_prim = MST.maximumSpanningTree(prim);
-		System.out.println(mst_prim);
-		
-
 		
 		}	
 	}
-
-
